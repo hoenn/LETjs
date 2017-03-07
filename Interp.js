f95bd82398c2158f559720a7a479fc70d38c02dc
emena <evan.mena1@marist.edu>
2017-03-07 12:37:15 -0500

    Added interpretation for ExpVal

--- Interp.js
+++ Interp.js
@@ -10,12 +10,12 @@ function valueOf (e, p) {
                 return ENV.applyEnv(p, e.Id);
                 break;
         case AST.IsZeroExp:
-                return new VAL.BoolVal(valueOf(e.Exp, p) == 0);
+                return new VAL.BoolVal(valueOf(e.Exp, p).val == 0);
                 break;
         case AST.DiffExp: 
-                var n1 = new VAL.NumVal(valueOf(e.Exp1,p));
-                var n2 = new VAL.NumVal(valueOf(e.Exp2, p));
-                return new VAL.NumVal(n1 - n2);
+                var n1 =valueOf(e.Exp1,p);
+                var n2 =valueOf(e.Exp2, p);
+                return new VAL.NumVal(n1.val - n2.val);
                 break;
         case AST.LetExp:
                 var body = e.Exp2;
@@ -24,7 +24,7 @@ function valueOf (e, p) {
                 return valueOf(body, pp);
                 break;
         case AST.IfExp: 
-                var test = new VAL.BoolVal(valueOf(e.Exp1, p));
+                var test = valueOf(e.Exp1, p);
                 if (test.val == true){
                     return valueOf(e.Exp2, p);
                 } else {
