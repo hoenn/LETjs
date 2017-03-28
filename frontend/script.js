2735b1615ca615fcd2b00a7955cdfb907365ba0e
Hoenn <skatevan1313@gmail.com>
2017-03-28 15:47:40 -0400

    Started work on rough html front end gui

---
+++ frontend/script.js
@@ -0,0 +1,16 @@
+var $ = require('jQuery');
+var INTERP = require("../src/Interp.js");
+var ENV = require("../src/Environment.js");
+var STO = require("../src/Store.js");
+var AST = require("../src/AST.js");
+var parser = require("../grammar/LET.js").parser;
+
+function parse(){
+    var input = $("#user").val();
+    var emptyEnv = ENV.emptyEnv();
+    var emptySto = new STO.Store();
+    var output = parser.parse(input);
+    var pgm = new AST.Pgm(output);
+    $("#output").text(INTERP.valueOf(pgm, emptyEnv, emptySto));
+}
+
