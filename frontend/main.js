c62a1cd5ce98e14dcd1578581981efae37838fc4
Hoenn <skatevan1313@gmail.com>
2017-03-28 22:01:57 -0400

    added browserify to dependencies, finished implementing bare minimum front end

--- frontend/main.js
+++ frontend/main.js
@@ -1,16 +1,18 @@
+
 var $ = require('jQuery');
+var util = require('util');
 var INTERP = require("../src/Interp.js");
 var ENV = require("../src/Environment.js");
 var STO = require("../src/Store.js");
 var AST = require("../src/AST.js");
 var parser = require("../grammar/LET.js").parser;
 
-function parse(){
+window.parse = function(){
     var input = $("#user").val();
     var emptyEnv = ENV.emptyEnv();
     var emptySto = new STO.Store();
     var output = parser.parse(input);
     var pgm = new AST.Pgm(output);
-    $("#output").text(INTERP.valueOf(pgm, emptyEnv, emptySto));
+    $("#output").text(util.inspect(INTERP.valueOf(pgm, emptyEnv, emptySto)));
 }
 
