7e51f78044eb7ecb76f3b6f2d1e5f788a082c15c
Hoenn <skatevan1313@gmail.com>
2017-03-29 13:07:45 -0400

    Removed RefVal, not available if refs are implicity

--- src/Val.js
+++ src/Val.js
@@ -38,19 +38,10 @@ ProcVal.prototype.show = function ProcValShow(){
     return this.val;
 }
 
-var RefVal = function(ref){
-    this.val = ref
-}
-RefVal.prototype = Object.create(ExpVal.prototype);
-RefVal.prototype.constructor = RefVal;
-RefVal.prototype.show = function RefValShow(){
-    return this.val;
-}
 
 module.exports = {
     ExpVal: ExpVal,
     NumVal: NumVal,
     BoolVal: BoolVal,
-    ProcVal: ProcVal,
-    RefVal: RefVal
+    ProcVal: ProcVal
 };
